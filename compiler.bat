82ac29a8cf8251e8945a513580c242412b82a123
SegoCode <35817798+SegoCode@users.noreply.github.com>
2022-04-25 14:08:03 +0200

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -11,7 +11,7 @@
 ::  You need golang.org/x/sys/unix dependence in your system if
 ::  you want to compile for linux, run 'go get golang.org/x/sys/unix'
 ::
-::  For compiling whiout go.mod file set GO111MODULE to off, this
+::  For compiling without go.mod file set GO111MODULE to off, this
 ::  isnt recommended, if you dont have go.mod run 'go mod init main'
 ::  and 'go mod tidy' for create the go.mod file
 ::
